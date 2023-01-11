bc3e63e2ca38cfd39d85554f7b35bb9e2b5fcf34
Mark Hay <52739488+ml-markhay@users.noreply.github.com>
2023-01-11 22:39:19 +0000

    Update mlcli.js
    Support for additional APIs and methods

--- mlcli.js
+++ mlcli.js
@@ -4,7 +4,6 @@
 const fs = require("fs");
 const ini = require("ini");
 const request = require('request');
-//const https = require('https');
 
 class Config {
   constructor() {
@@ -61,20 +60,22 @@ function callback(error,respone) {
 	console.log(response);
 }
 
-function requestPOST(params) {
+function requestGET(api_name,api_path,params) {
 
 	//console.log(JSON.stringify(params))
 	
+	for (const [key, value] of Object.entries(params)) api_path = api_path.replace('{'+key+'}',encodeURIComponent(value));
+	
 	var options = {
 	  'encoding': null,
-	  'method': 'POST',
-	  'url': 'https://api.melroselabs.com/voice/tts/',
+	  'method': 'GET',
+	  'url': 'https://api.melroselabs.com/'+api_name+api_path+'/',
 	  'headers': {
-	    'x-api-key': config.credentials.api_key,
-	    'Content-Type': 'application/json'
-	  },
-	  body: JSON.stringify(params)
+	    'x-api-key': config.credentials.api_key
+	  }
 	};
+	
+	//console.log(options);
 	
 	request(options, function (error, response) { 
 	  
@@ -91,7 +92,44 @@ function requestPOST(params) {
 			});
 	  }
 	  
-	  if ( response.caseless.get('Content-Type') == 'application/json' ) console.log( response.body );
+	  if ( response.caseless.get('Content-Type') == 'application/json' ) console.log( response.body.toString() );
+
+	});
+}
+
+function requestPOST(api_name,api_path,params) {
+
+	//console.log(JSON.stringify(params))
+	
+	var options = {
+	  'encoding': null,
+	  'method': 'POST',
+	  'url': 'https://api.melroselabs.com/'+api_name+api_path+'/',
+	  'headers': {
+	    'x-api-key': config.credentials.api_key,
+	    'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify(params)
+	};
+	
+	//console.log(options);
+	
+	request(options, function (error, response) { 
+	  
+	  if (error) throw new Error(error);
+	  
+	  if ( response.caseless.get('Content-Type') == 'audio/mp3' )
+	  {
+		  // response is of type audio/mp3
+		  let filename = "out.mp3";
+		  //console.log(response)
+		  fs.writeFile(filename, response.body, "binary", (err) => {
+			  if (err) throw err;
+			  console.log("File saved to "+filename);
+			});
+	  }
+	  
+	  if ( response.caseless.get('Content-Type') == 'application/json' ) console.log( response.body.toString() );
 
 	});
 }
@@ -168,6 +206,12 @@ if ((process.argv.length<3)||((process.argv.length==3)&&((process.argv[2] == "he
 const config = new Config();
 
 if (process.argv[2] == "setup") { // setup <api-key>
+
+	if (process.argv.length<=3) {
+		console.log("Missing API key");
+		return;
+	}
+
 	// only download API definitions if key currently unset
 	var performDownloadAPIDefinitions = !fs.existsSync(config.readFilename());
 	// write API key
@@ -181,6 +225,13 @@ if (process.argv[2] == "setup") { // setup <api-key>
 if (process.argv[2] == "update") { // update (download) API definitions
 	// download API definitions
 	updateAPIDefinitions();
+	return;
+}
+
+if (process.argv[2] == "clear") { // clear configuration
+	// write API key
+	config.clear()
+	console.log("API key cleared")
 	return;
 }
 
@@ -212,6 +263,7 @@ if (((process.argv.length==4)&&(process.argv[3] == "help"))||(process.argv.lengt
 		if (key[0]=="/") service = key.substr(1);
 		
 		if (value["post"]) console.log(" "+service.padEnd(20," ")+": "+value["post"]["summary"]);
+		if (value["get"]) console.log(" "+service.padEnd(20," ")+": "+value["get"]["summary"]);
 	}
 	console.log("");
 
@@ -228,7 +280,6 @@ if (((process.argv.length==5)&&(process.argv[4] == "help"))||(process.argv.lengt
 		return;
 	}
 	
-	
 	var textschema = fs.readFileSync(config.apiDefinitionFilename(api_name), 'utf-8');
 	var schema = JSON.parse(textschema)
 	//console.log(schema["paths"])
@@ -240,24 +291,62 @@ if (((process.argv.length==5)&&(process.argv[4] == "help"))||(process.argv.lengt
 		return;
 	}
 	
-	console.log("Usage: melroselabs-cli "+api_name+" "+process.argv[3]+" create <parameters>\n");
+	//
 	
-	console.log("Where <parameters> are:");
+	api_method = "";
 	
-	var api_method = "post";
-		
-	var rest_api_param_definition_name = schema["paths"][api_path][api_method]["parameters"][0]["name"];
-	var rest_api_param_definition = schema["definitions"][rest_api_param_definition_name];
-	var rest_api_param_definition_required = rest_api_param_definition["required"]
+	// create
+	
+	api_method = "post";
+	
+	if (rest_api_param_definition_name = schema["paths"][api_path][api_method]!=null)
+	{
 
-	for (const [key, value] of Object.entries(rest_api_param_definition["properties"])) {
-		console.log(" "+key.padEnd(16," ") + " : " + value["description"]);
-		if (value["enum"]) {
-			let str = "                    Choices:";
-			let i = 0;
-			for(let a of value["enum"]) { if (i%8==0) str += "\n                      "; i++; str += a.padEnd(16," "); }
-			console.log(str)
+		console.log("Usage: melroselabs-cli "+api_name+" "+process.argv[3]+" create <parameters>\n");
+		
+		console.log("Where <parameters> are:");
+		
+		var rest_api_param_definition_name = schema["paths"][api_path][api_method]["parameters"][0]["name"];
+		var rest_api_param_definition = schema["definitions"][rest_api_param_definition_name];
+		var rest_api_param_definition_required = rest_api_param_definition["required"]
+	
+		for (const [key, value] of Object.entries(rest_api_param_definition["properties"])) {
+			console.log(" "+key.padEnd(16," ") + " : " + value["description"]);
+			if (value["enum"]) {
+				let str = "                    Choices:";
+				let i = 0;
+				for(let a of value["enum"]) { if (i%8==0) str += "\n                      "; i++; str += a.padEnd(16," "); }
+				console.log(str)
+			}
 		}
+	
+	}
+	
+	// get
+
+	api_method = "get";
+	
+	if (rest_api_param_definition_name = schema["paths"][api_path][api_method]!=null)
+	{
+	
+		console.log("Usage: melroselabs-cli "+api_name+" "+process.argv[3]+" create <parameters>\n");
+		
+		console.log("Where <parameters> are:");
+		
+		var rest_api_param_definition_name = schema["paths"][api_path][api_method]["parameters"][0]["name"];
+		var rest_api_param_definition = schema["definitions"][rest_api_param_definition_name];
+		var rest_api_param_definition_required = rest_api_param_definition["required"]
+	
+		for (const [key, value] of Object.entries(rest_api_param_definition["properties"])) {
+			console.log(" "+key.padEnd(16," ") + " : " + value["description"]);
+			if (value["enum"]) {
+				let str = "                    Choices:";
+				let i = 0;
+				for(let a of value["enum"]) { if (i%8==0) str += "\n                      "; i++; str += a.padEnd(16," "); }
+				console.log(str)
+			}
+		}
+	
 	}
 	
 	//
@@ -271,13 +360,6 @@ if (((process.argv.length==5)&&(process.argv[4] == "help"))||(process.argv.lengt
 
 if (!fs.existsSync(config.readFilename())) {
 	console.log("API key not found. Get key from melroselabs.com.\n\nAdd using:\n\n  melroselabs-cli setup <api-key>\n");
-	return;
-}
-
-if (process.argv[2] == "clear") { // clear configuration
-	// write API key
-	config.clear()
-	console.log("API key cleared")
 	return;
 }
 
@@ -298,10 +380,16 @@ var textschema = fs.readFileSync(config.apiDefinitionFilename(api_name), 'utf-8'
 var schema = JSON.parse(textschema)
 //console.log(schema)
 
+//console.log(schema["paths"]);
+
 var api_path = "/"+process.argv[3]
+
+if (api_path.indexOf("/{")!=-1) api_path.substr(0,api_path.indexOf("/{"));
+
 //console.log(api_path)
+
 if (schema["paths"][api_path] == null) {
-	console.log("Unknown path: "+process.argv[3]);
+	console.log("Unknown path : "+process.argv[3]);
 	return;
 }
 
@@ -314,17 +402,39 @@ if (schema["paths"][api_path][api_method] == null) {
 	return;
 }
 
-var rest_api_param_definition_name = schema["paths"][api_path][api_method]["parameters"][0]["name"];
-//console.log(rest_api_param_definition_name)
+var rest_api_param_definition_required = []; //empty array of required parameters
 
-var rest_api_param_definition = schema["definitions"][rest_api_param_definition_name];
-//console.log(rest_api_param_definition)
+var rest_api_param_definition_name = null;
+for(var i=0;i<schema["paths"][api_path][api_method]["parameters"].length;i++) {
 
-var rest_api_param_definition_required = rest_api_param_definition["required"]
+	rest_api_param_definition_name = schema["paths"][api_path][api_method]["parameters"][i]["name"];
+	//console.log(rest_api_param_definition_name)
+	
+	if (schema["paths"][api_path][api_method]["parameters"][i]["schema"]) {
+	
+		// get required status from "definition" section
+	
+		var rest_api_param_definition = schema["definitions"][rest_api_param_definition_name];
+		//console.log(rest_api_param_definition)
+		
+		if (rest_api_param_definition) {
+			if (rest_api_param_definition["required"] != null) rest_api_param_definition_required = rest_api_param_definition_required.concat( rest_api_param_definition["required"] );
+		}
+	
+	}
+	else if (schema["paths"][api_path][api_method]["parameters"][i]["required"] === true) {
+		rest_api_param_definition_required.push(rest_api_param_definition_name);
+	}
+
+}
+
+//console.log("Required parameters:");
 //console.log(rest_api_param_definition_required)
 
-var api_params = [];
-var params = {};
+//
+
+var api_params = []; // array of labels for parameters from command-line
+var params = {}; // array of key-value pairs for parameters from command-line
 
 //console.log(process.argv.length)
 for(let i=5;i<process.argv.length;i++)
@@ -353,5 +463,5 @@ if (!required_params_present) return;
 
 //
 
-if (api_method == "post") requestPOST(params)
-
+if (api_method == "post") requestPOST(api_name,api_path,params)
+if (api_method == "get") requestGET(api_name,api_path,params)
